1c5d1c04b4fcd53aa115227e5f2b07e4cc65d582
abraham <abraham@newverveconsulting.com>
2022-09-26 10:31:27 +0100

    making repo standard template

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import ForgeUI, {
+  Fragment,
+  GlobalPage,
+  Text,
+  useState,
+  useEffect,
+} from '@forge/ui';
+
+export default function () {
+  return (
+    <GlobalPage>
+      <Fragment>
+        <Text>Hello World!</Text>
+      </Fragment>
+    </GlobalPage>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import Resolver from '@forge/resolver';
+import ForgeUI, { render } from '@forge/ui';
+import App from './App';
 
+// redirectig to UI kit page
+export const Global = render(
+  <App />
+);
+
+// backend "forge lambda" to be called by frontend React for instance
 const resolver = new Resolver();
-
 resolver.define('getText', (req) => {
   console.log(req);
 
   return 'Hello, this is some data for the world!';
 });
-
 export const handler = resolver.getDefinitions();
